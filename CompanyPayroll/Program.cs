c464989edb2cb1ca1275f8769eac36a1e3d513b7
Benjamin Williams <33302778+Dean110@users.noreply.github.com>
2020-05-27 14:26:30 -0400

    Initial commit

---
+++ CompanyPayroll/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CompanyPayroll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
