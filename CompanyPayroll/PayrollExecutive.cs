d60cf24542d701555f8c1367ac752f5687347653
Daniel Roman <dan.roman21@gmail.com>
2020-05-28 14:27:59 -0400

    Added PayrollExecutive class

---
+++ CompanyPayroll/PayrollExecutive.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompanyPayroll
+{
+    class PayrollExecutive : PayrollEmployee
+    {
+
+        public double PayPeriodEarnings = 20000.00;
+        private double InsuranceAmount = 10.00;
+        public void RecieveBonus(double BonusAmount)
+        {
+            this.EmployeeBonus = BonusAmount * 3;
+        }
+
+        public void PayInsurance()
+        {
+            this.PayPeriodEarnings -= InsuranceAmount;
+        }
+
+        public override void SetPaycheckTotal()
+        {
+            this.PaycheckTotal = PayPeriodEarnings + EmployeeBonus;
+        }
+
+    }
+}
