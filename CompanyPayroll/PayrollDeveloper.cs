c1dc9e7ad28571337f28f75980b49881ed6f1d82
Bernard828 <bernard.lackey1@gmail.com>
2020-05-28 14:17:35 -0400

    added payroll developer class

---
+++ CompanyPayroll/PayrollDeveloper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompanyPayroll
+{
+    class PayrollDeveloper : PayrollEmployee
+    {
+        public double PayPeriodEarnings = 9000.00;
+        private double InsuranceAmount = 80.00;
+        public void RecieveBonus(double BonusAmount)
+        {
+            this.EmployeeBonus = BonusAmount;
+        }
+
+        public void PayInsurance()
+        {
+            this.PayPeriodEarnings -= InsuranceAmount;
+        }
+
+        public override void SetPaycheckTotal()
+        {
+            this.PaycheckTotal = PayPeriodEarnings + EmployeeBonus;
+        }
+    }
+}
