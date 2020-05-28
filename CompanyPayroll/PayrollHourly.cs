f0c56b8924f4f052aaba48107f3c9b31398d2296
Tom <tomrambo95@yahoo.com>
2020-05-28 11:24:40 -0400

    added payrollHourly class

---
+++ CompanyPayroll/PayrollHourly.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompanyPayroll
+{
+    class PayrollHourly : PayrollEmployee
+    {
+        public override void SetPaycheckTotal()
+        {
+            this.PaycheckTotal = HoursWorked * HourlyRate;
+        }
+        private int HoursWorked;
+        private double HourlyRate = 20.00;
+
+        public void SetHoursWorked(int newhoursWorked)
+        {
+            this.HoursWorked = newhoursWorked;
+        }
+
+        public void SetHourlyRate(double newhourlyRate)
+        {
+            this.HourlyRate = newhourlyRate;
+        }
+    }
+}
