2c73bb6d640511c277394b79616aa16eb91a27ca
Daniel Roman <dan.roman21@gmail.com>
2020-05-28 10:17:54 -0400

    Set up initial class template

---
+++ CompanyPayroll/PayrollEmployee.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompanyPayroll
+{
+    abstract class PayrollEmployee
+    {
+        private int employeeId;
+        String firstName;
+        String lastName;
+        double paycheckTotal;
+
+        public int EmployeeID
+        { 
+            get { return this.employeeId;  }
+            set { this.employeeId = value;  }
+        
+        }
+        public string FirstName
+        {
+            get { return this.firstName; }
+            set { this.firstName = value; }
+        }
+        public string LastName
+        {
+            get { return this.lastName; }
+            set { this.lastName = value; }
+        }
+        public double PaycheckTotal
+        {
+            get { return this.paycheckTotal; }
+            //set { this.paycheckTotal = value; }
+        }
+
+         
+
+        abstract public void SetPaycheckTotal();
+
+
+        
+    }
+    
+}
---
+++ CompanyPayroll/PayrollSales.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompanyPayroll
+{
+    class PayrollSales : PayrollEmployee
+    {
+        public override void SetPaycheckTotal()
+        {
+            this.GetEmployeeID; 
+
+
+            throw new NotImplementedException();
+        }
+        
+    }
+}
